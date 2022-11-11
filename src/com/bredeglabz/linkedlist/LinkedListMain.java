c95fc64a80af0583fabd7a82c20b30812ff1b7d2
pratikraut25 <pratikraut2512@gmail.com>
2022-11-11 22:27:43 +0530

    createlist

---
+++ src/com/bredeglabz/linkedlist/LinkedListMain.java
@@ -0,0 +1,15 @@
+package com.bredeglabz.linkedlist;
+
+public class LinkedListMain {
+
+	public static void main(String[] args) {
+		 System.out.println("---------Welcome to the LinkedList program---------");
+	        LinkedList<Integer> list1 = new LinkedList();
+	        System.out.println("--------------Create Linked List----------------");
+	        list1.push(56);
+	        list1.push(30);
+	        list1.push(70);
+	        list1.print();
+	}
+
+}
